8856fc574e1ef6c3a26a0c4871a35c654336f581
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-02 07:34:13 -0700

    betty hates trailing white spaces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,16 @@
 #include "stdio.h"
-int main(void) 
+
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: using puts insted of printf to print something on the screen
+ *
+ * Return: always returns (0)
+ */
+
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
