765e6afe3f4530a86427274b11590113d38d02b8
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-02 08:03:39 -0700

    betty wants me to abid her standards

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,14 +4,14 @@
 /**
  * main - Entry point in this program
  *
- * Description - using printf to print some text on the screen
+ * Description: using printf to print some text on the screen
  *
- * Return - always return (0)
+ * Return: always return (0)
  */
 
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
