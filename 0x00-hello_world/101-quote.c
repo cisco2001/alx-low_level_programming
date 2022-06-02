ace028c6d1b5ee6ebc91eb1b8c6e829adb988369
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-02 09:49:01 -0700

    printing to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+
+/**
+ * main - Entry point for this program
+ *
+ * Description: printing text string in stderr
+ *
+ * Return: always returns (1)
+ */
+
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is");
+	fprintf(stderr, "useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
