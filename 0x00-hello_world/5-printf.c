9412b9e7f7d5c718910650f1f55f5a56844dbc17
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-02 07:48:09 -0700

    printing with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+
+/**
+ * main - Entry point in this program
+ *
+ * Description - using printf to print some text on the screen
+ *
+ * Return - always return (0)
+ */
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
