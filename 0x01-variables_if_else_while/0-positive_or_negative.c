969f96600735c9f72852c0529489487be6d9fec9
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-03 15:01:33 -0700

    let me be positive

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+#include "time.h"
+
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: a program to check whether a number is +/- or zero
+ *
+ * Return: always return zero
+ */
+
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		fprintf(stdout, "%d is positive\n", n);
+	else if (n < 0)
+		fprintf(stdout, "%d is negative\n", n);
+	else
+		fprintf(stdout, "%d is zero\n", n);
+	return (0);
+}
