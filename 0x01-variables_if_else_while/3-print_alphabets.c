3ac47c6f9842c8a11da3fb2da083cf546885b47a
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-05 13:41:16 -0700

    always start small

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,13 +13,13 @@ int main(void)
 {
 	char uppercase = 65, lowercase = 97;
 
-	while (uppercase <= 90)
-	{
-		putchar(uppercase++);
-	}
 	while (lowercase <= 122)
 	{
 		putchar(lowercase++);
+	}
+	while (uppercase <= 90)
+	{
+		putchar(uppercase++);
 	}
 	putchar((int)'\n');
 	return (0);
