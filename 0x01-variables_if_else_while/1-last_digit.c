4daf9a0f45e5f326466b38e1842ba2879b29e0ce
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-05 11:45:26 -0700

    another try

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,13 +22,19 @@ int main(void)
 	int lastDigit = n % 10;
 
 	if (lastDigit > 5)
+	{
 		fprintf(stdout, "%s of %d is %d", string, n, lastDigit);
 		fprintf(stdout, " and is greater than 5\n");
+	}
 	if (lastDigit == 0)
+	{
 		fprintf(stdout, "%s of %d is %d", string, n, lastDigit);
 		fprintf(stdout, " and is zero\n");
-	else if (lastDigit < 6)
+	}
+	if (lastDigit < 6)
+	{
 		fprintf(stdout, "%s of %d is %d", string, n, lastDigit);
 		fprintf(stdout, " and is less than 6 and not 0\n");
+	}
 	return (0);
 }
