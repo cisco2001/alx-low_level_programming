edd38b1595787fdd78cb9bae3e7ce62e5dc58fa8
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-05 15:03:12 -0700

    16 base again

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,16 +13,25 @@
 int main(void)
 {
 	int hex_number = 0;
-	int ascii_0 = 48, ascii_a = 97;
+	int ascii_0 = 48, ascii_a = 87;
 
 	while (hex_number <= 16)
 	{
 		if (hex_number <= 9)
-			putchar((hex_number++) + (ascii_0++));
+		{
+			putchar(hex_number + ascii_0);
+			ascii_0++;
+		}
 		else
-			putchar((hex_number++) + (ascii_a++));
+		{
+			putchar(hex_number + ascii_a);
+			ascii_a++;
+		}
 
-		putchar((int)'\n');
+		hex_number++;
+
+		if (hex_number > 16)
+			putchar((int)'\n');
 	}
 
 	return (0);
