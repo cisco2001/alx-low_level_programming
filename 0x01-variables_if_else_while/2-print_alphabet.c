f82c6c1ee2310bd8831397020ca0a441a319414a
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-05 12:45:20 -0700

    printing all alphabets in small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+
+
+/**
+ * main - Entry point of this program
+ *
+ * Description: a program to print characters
+ *
+ * Return: always returns (0)
+ */
+
+int main(void)
+{
+	char character = 97;
+
+	while (character < 123)
+	{
+		putchar(character);
+		putchar((int)'\n');
+		character++;
+	}
+
+	return (0);
+}
