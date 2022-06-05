f817a372939b8bf091f6edd2a5c7e2c75fbcaf18
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-05 13:34:49 -0700

    printing characters in both uppercase and lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: a program that prints all letters in uppercase and lowercase
+ *
+ * Return: always returns(0)
+ */
+
+int main(void)
+{
+	char uppercase = 65, lowercase = 97;
+
+	while (uppercase <= 90)
+	{
+		putchar(uppercase++);
+	}
+	putchar((int)'\n');
+	while (lowercase <= 122)
+	{
+		putchar(lowercase++);
+	}
+	return (0);
+}
