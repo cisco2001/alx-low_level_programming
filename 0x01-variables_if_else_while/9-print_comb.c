73dc98fc400f95ace6e588f1f5c89cb5cebd6d65
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-06 09:17:21 -0700

    single digits to be printed

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+
+
+/**
+ * main - Entry point of this program
+ *
+ * Description: a program to print all single digits in ascending order
+ *
+ * Return: always return (0)
+ */
+
+int main(void)
+{
+	int count = 0;
+
+	while (count <= 9)
+	{
+		putchar(48 + count);
+		count++;
+		if (count <= 9)
+		{
+			putchar((int)',');
+			putchar((int)' ');
+		}
+	}
+
+	return (0);
+}
