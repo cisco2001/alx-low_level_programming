cb8ce1a67bdfb260932676687643bd70ed9fd23b
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-07 13:09:09 -0700

    ready to print 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet_x10 - function that prints lowercase alphabets 10 times
+ *
+ * Description: a program to print lowercase alphabets 10 times
+ *
+ * Return: nothing is returned
+ */
+
+
+void print_alphabet_x10(void)
+{
+	char alphabet = 97;
+	int number_of_times = 0;
+
+	for (; number_of_times <= 9; number_of_times++)
+	{
+		while (alphabet <= 122)
+		{
+			_putchar(alphabet);
+			alphabet++;
+			if (alphabet > 122)
+				_putchar((int)'\n');
+		}
+	}
+}
