783bea2bb79160d7adfe834ac70e6cce43a5d597
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-07 14:59:35 -0700

    I have to finish this even if I will be the last man standing

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * print_last_digit - functionthat prints last digit of a number
+ *
+ * @number: a parameter to take ina number
+ *
+ * Description: a program that prints the last digit of a number
+ *
+ * Return: last digit of @number
+ */
+
+int print_last_digit(int number)
+{
+	return (number % 10);
+}
