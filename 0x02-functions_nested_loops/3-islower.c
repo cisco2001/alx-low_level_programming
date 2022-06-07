ec4af5376d0f243cdedb91e2ae491db74ab4af6a
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-07 13:39:42 -0700

    done with betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,11 +6,13 @@
  *
  * Description: a program to check for lowercase characters
  *
+ * @c: a parameter for this function
+ *
  * Return: can return (0) or (1)
  */
 
 
 int _islower(int c)
 {
-	return (c >= 97 && c <= 122) ? (1) : (0);
+	return ((c >= 97 && c <= 122) ? (1) : (0));
 }
