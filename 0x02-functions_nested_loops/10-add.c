50bcc8e9185547cb1cd8ec5b7af480b97a73046d
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-08 00:58:13 -0700

    just sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * add - function that adds two integers and returns the result
+ *
+ * @number1: first parameter to take in number
+ *
+ * @number2: second parameter to take in number
+ *
+ * Descriptio: program to add two integers and returns the result
+ *
+ * Return: result of addition of two integers
+ */
+
+
+int add(int number1, int number2)
+{
+	return (number1 + number2);
+}
