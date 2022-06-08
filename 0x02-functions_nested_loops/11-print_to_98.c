91823e6cc6b70a94a21b666eb1550f4d4f054f47
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-08 01:11:11 -0700

    from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * print_to_98 - function to print from n to 98
+ *
+ * @n: a number, starting point
+ *
+ * Description: program to print numbers in ascending order from n to 98
+ *
+ * Return: nothing is returned
+ */
+
+
+void print_to_98(int n)
+{
+	for (; n <= 98; n++)
+	{
+		printf("%d", n);
+		if (n < 98)
+			printf(", ");
+	}
+}
