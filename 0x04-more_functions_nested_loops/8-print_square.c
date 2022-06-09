28d4df65cfddf9bb1eab5dab02c26953a521b3ea
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-09 16:37:22 -0700

    another shot

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,21 +4,25 @@
 /**
  * print_square - function to print a square
  *
- * @size: parameter that stands for square size
+ * @size: parameter for passing size of the square
  *
  * Return: nothing is returned
  */
 
-
 void print_square(int size)
 {
-	if (size >= 0)
+	if (size <= 0)
 	{
-		int side1_length = 0
+		_putchar((int)'\n');
+	}
+	else
+	{
+		int side1_length = 0;
+		int side2_length;
 
 		for (; side1_length < size; side1_length++)
 		{
-			int side2_length = 0;
+			side2_length = 0;
 
 			for (; side2_length < size; side2_length++)
 			{
@@ -27,6 +31,4 @@ void print_square(int size)
 			_putchar((int)'\n');
 		}
 	}
-	else
-		_putchar((int)'\n');
 }
