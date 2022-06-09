e26da6d703232c127eeba74c2c464dfebe374b1f
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-09 05:35:26 -0700

    time to multiply a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * mul - function that multipies two integers
+ *
+ * @a: parameter for passing the first integer
+ *
+ * @b: parameter for passing the second integer
+ *
+ * Description: program to calculate product of @a and @b
+ *
+ * Return: product of @a and @b
+ */
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
