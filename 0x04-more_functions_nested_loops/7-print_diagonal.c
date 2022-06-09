f1897f6f88e738ee8cb35d9869e4201715f505da
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-09 08:11:26 -0700

    I am citiuniverse

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+/**
+ * print_diagonal - functiopn to print a diagonal in a screen
+ *
+ * @n: parameter for passing the numbe rof times character '\' has to be printed
+ *
+ * Return: nothing is returned
+ */
+
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar((int)'\n');
+	else
+	{
+		int count = 0;
+
+		for (; count < n; count++)
+		{
+			int number_of_spaces = 0;
+
+			for (; number_of_spaces < count; number_of_spaces++)
+			{
+				_putchar((int)' ');
+			}
+
+			_putchar((int)'\');
+			_putchar((int)'\n');
+		}
+	}
+}
