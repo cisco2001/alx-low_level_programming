6a65d7970ba1fe3d8b14e965216e5ec929587786
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-13 05:22:16 -0700

    journey to thousands of pointers begin with a single pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * reset_to_98 - updates the value it points to 98
+ *
+ * @n: a pointer
+ *
+ * Description: function that has a pointer parameter
+ *
+ * Return:nothing is returned
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
