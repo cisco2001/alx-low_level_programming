cac59d64ae6ea8a3cdb1724201ad269a809decd0
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-06-13 15:15:20 -0700

    today I must finish this

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,3 +11,16 @@
 
 
 void print_rev(char *s)
+{
+	char *start_string = s;
+
+	while (*s != '\0')
+	{
+		s++;
+	}
+	for (; s >= start_string; s--)
+	{
+		_putchar(*s);
+	}
+	_putchar((int)'\n');
+}
