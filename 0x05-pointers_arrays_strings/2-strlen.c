7da282c15a0c51f54e784d5d9bf24b028280cd6b
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-07-19 21:17:45 -0700

    fixing some issues with betty

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,15 +3,14 @@
 /**
  * _strlen - computes length of a given string
  * @s: holds address of the first character
- * 
+ *
  * Return: length of a string;
  */
 
 int _strlen(char *s)
 {
 	int string_length = 0;
-
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		string_length += 1;
 		s += 1;
