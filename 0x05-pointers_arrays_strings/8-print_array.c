a6228348fb5a80d338d76134c05c113062196b6c
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-07-20 00:51:22 -0700

    fixed minor issue

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
-#include "main"
-/**
+#include "main.h"
 
+/**
  * print_array - prints n elements of an array of integers
  * @a: holds address of the first element of an array
  * @n: number of elements to be printed
