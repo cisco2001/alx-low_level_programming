6657a79203a3e96b47d43d8c076af60469a4c82b
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-08-22 22:02:29 +0300

    added code to check size of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+/**
+ * main - main entry of the program
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+  printf("Size of a char: %d\n", sizeof(char));
+  printf("size of an int: %d\n", sizeof(int));
+  printf("size of a long: %d\n", sizeof(long int));
+  printf("size of a long long int: %d\n", sizeof(long long int));
+  printf("size of a float: %d\n", sizeof(float));
+  return (0);
+}
