652e5983ff7f45f02e5bd0afb635220b5beb2971
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-08-22 21:13:08 +0300

    used 'puts' to print string in stdout

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+/**
+ * main - entry of the program
+ *
+ * Return: zero will be returned
+ */
+
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle");
+  puts("\n");
+  return (0);
+}
