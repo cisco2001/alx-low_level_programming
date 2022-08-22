5978d1d45e6a8b757a241ea390db31a7a3f1a3c5
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-08-22 22:08:57 +0300

    modified strings

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-  printf("Size of a char: %d\n", sizeof(char));
-  printf("size of an int: %d\n", sizeof(int));
-  printf("size of a long: %d\n", sizeof(long int));
-  printf("size of a long long int: %d\n", sizeof(long long int));
-  printf("size of a float: %d\n", sizeof(float));
+  printf("Size of a char: %d byte(s)\n", sizeof(char));
+  printf("size of an int: %d byte(s)\n", sizeof(int));
+  printf("size of a long: %d byte(s)\n", sizeof(long int));
+  printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+  printf("size of a float: %d byte(s)\n", sizeof(float));
   return (0);
 }
