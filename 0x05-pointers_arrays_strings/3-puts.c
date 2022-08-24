9705342447a31de0ee7dff6a42e7204b2d323a46
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-08-24 08:22:46 -0700

    ready to print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _puts - functon that prints a string followed by a new line
+  *
+  * @str: pointer to char
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str = (str + 1);
+		if (*str == '\0')
+			_putchar(0);
+	}
+}
