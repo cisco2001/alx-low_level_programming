858e045a221e35edeae63e3e988e9d21e9e3464b
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-08-24 11:00:25 -0700

    ready to reverse any string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_rev - function that prints a reversed string
+  *
+  * @s: pointer to char
+ */
+
+void print_rev(char *s)
+{
+	char *char_address = (s + _strlen((*s)) - 1);
+
+	while (1)
+	{
+		if (char_address >= s)
+		{
+			_putchar((char_address));
+			char_address = (char_address - 1);
+		}
+		else
+			_putchar(10);
+	}
+}
+
