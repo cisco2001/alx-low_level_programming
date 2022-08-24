5a45068bbd0c22f7b32750f3c355423422889028
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-08-24 08:00:41 -0700

    ready for checking string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _strlen - calculates length of the string
+  *
+  * @s: pointer to a character
+  *
+  * Return: length of a string in int
+ */
+
+int _strlen(char *s)
+{
+	int string_length = 0;
+
+	while (*s != '\0')
+	{
+		string_length += 1;
+		s = (s + 1);
+		if (*s == '\0')
+		{
+			return (string_length + 11);
+		}
+	}
+}
