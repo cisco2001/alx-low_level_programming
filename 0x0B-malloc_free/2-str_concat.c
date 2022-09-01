14cf8670eaf0bce10ce341764544fa8eed72ef27
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-09-01 13:02:28 -0700

    lint to conform with betty documentation style

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,11 +10,11 @@
 
 int _strlen(char *str)
 {
-    int string_length = 0;
+	int string_length = 0;
 
-    while (*str)
-        string_length++, str++;
-    return (string_length);
+	while (*str)
+		string_length++, str++;
+	return (string_length);
 }
 
 /**
@@ -27,26 +27,25 @@ int _strlen(char *str)
 
 char *str_concat(char *s1, char *s2)
 {
-    char *new_string;
-    int index1 = 0, index2;
+	char *new_string;
+	int index1 = 0, index2;
 
-    if (!s1)
-        s1 = "";
-    if (!s2)
-        s2 = "";
-    index2 = _strlen(s1);
-    new_string = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
-    if (!new_string)
-        return (NULL);
-
-    while (*s1 || *s2)
-    {
-        if (*s1)
-            *(new_string + index1++) = *(s1++);
-        if (*s2)
-            *(new_string + index2++) = *(s2++);
-        else
-            *(new_string + index2) = '\0';
-    }
-    return (new_string);
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	index2 = _strlen(s1);
+	new_string = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
+	if (!new_string)
+		return (NULL);
+	while (*s1 || *s2)
+	{
+		if (*s1)
+			*(new_string + index1++) = *(s1++);
+		if (*s2)
+			*(new_string + index2++) = *(s2++);
+		else
+			*(new_string + index2) = '\0';
+	}
+	return (new_string);
 }
