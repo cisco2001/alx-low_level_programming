a53c182044a618acfbd680313a0d0e8026ecf0c6
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-09-01 13:32:03 -0700

    matrix with pointers

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+  * alloc_grid - creates a matrix
+  *
+  * @width: number of columns
+  * @height: number of rows
+  * Return: address of the location containing matrix
+  */
+
+int **alloc_grid(int width, int height)
+{
+	int **matrix;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	matrix = calloc(sizeof(int) * width * height);
+	if (!matrix)
+		return (NULL);
+	return (matrix);
+}
+
