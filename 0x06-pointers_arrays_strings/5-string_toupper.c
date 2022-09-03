829904e8af366bd84d31d1e9516f89a041071c88
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-09-03 04:16:37 -0700

    fix betty style

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,10 +13,10 @@ char *string_toupper(char *s)
 	int index = 0;
 
 	while (*(s + index))
-  {
-    if (*(s + index) >= 97 && *(s + index) <= 122)
+	{
+		if (*(s + index) >= 97 && *(s + index) <= 122)
 			*(s + index) = *(s + index) - 32;
-    index++;
-  }
+		index++;
+	}
 	return (s);
 }
