1137e1b0edbad327def6373f8cd18efc9766a1dc
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-09-03 00:43:01 -0700

    add function to join two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * _strlen - calculates string length
+  *
+  * @str: pointer to the string
+  * Return: string length
+  */
+
+int _strlen(char *str)
+{
+	int string_length = 0;
+
+	while (*str)
+		string_length++, str++;
+	return (string_length);
+}
+
+/**
+  * _strncat - joins two strings
+  *
+  * @dest: pointer to the first string
+  * @src: pointer to the first string
+  * @n: number of bytes to be used
+  * Return: pointer of the resulting string
+  */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = _strlen(dest), j = 0;
+	while (n > 0)
+		*(dest + index++) = *(src + j++), n--;
+	return (dest);
+}
