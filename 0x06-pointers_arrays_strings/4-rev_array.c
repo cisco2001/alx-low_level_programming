1e3351fa3442088019293be736fd70b29796874c
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-09-03 02:41:21 -0700

    swap by XOR

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * reverse_array - reverses array content
+  *
+  * @a: an array of integers
+  * @n: number of elements to swap
+  * Return: nothing
+  */
+
+void reverse_array(int *a, int n)
+{
+	int temp, i = 0, j = n - 1;
+
+	while (i != j)
+	{
+		a[i] ^= a[j], a[j] ^= a[i], a[i] ^= a[j];
+		i++, j--;
+	}
+}
