6f5567e7e9858a2fb61cc718a4e4eb3273d5fa92
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-09-03 14:14:13 +0300

    everything is ok

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "stdio.h"
 
 /**
   * string_toupper - converts lowercase to uppercase
@@ -12,7 +13,10 @@ char *string_toupper(char *s)
 	int index = 0;
 
 	while (*(s + index))
-		if (*(s + index) >= 97 && *(s + index) <= 122)
-			*(s + index++) = *(s) - 32;
+  {
+    if (*(s + index) >= 97 && *(s + index) <= 122)
+			*(s + index) = *(s + index) - 32;
+    index++;
+  }
 	return (s);
 }
