69b9f0c165c6a5bb1b77e007dd085dde1dadb149
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-09-03 03:42:03 -0700

    use ASCII values to convert upprcase letters to lowercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * string_toupper - converts lowercase to uppercase
+  *
+  * @s: address of the string to manipulate
+  * Return: adress of the resulting string
+  */
+
+char *string_toupper(char *s)
+{
+	int index = 0;
+
+	while (*(s + index))
+		if (*s >= 97 && *s <= 122)
+			*(s + index++) = *s - 32;
+}
