0e92ea4072df0ecdc2662107cef174f00717258a
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-09-03 12:01:03 -0700

    declare dog data type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - fog data type
+  * @name: address of dog's name
+  * @owner: address of owner's name
+  * @age: age of the dog
+  */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
