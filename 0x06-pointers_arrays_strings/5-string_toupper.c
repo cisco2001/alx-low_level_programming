4c308d2c5d75c89703c80a8ac1c8e9639f2594c9
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-09-03 13:52:27 +0300

    add index to the pointer to point to the correct value

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,7 +12,7 @@ char *string_toupper(char *s)
 	int index = 0;
 
 	while (*(s + index))
-		if (*s >= 97 && *s <= 122)
+		if (*(s + index) >= 97 && *(s + index) <= 122)
 			*(s + index++) = *s - 32;
 	return (s);
 }
