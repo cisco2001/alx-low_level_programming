1bd03a20d5a6a79624202f84be7b822d377677c9
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-09-06 03:10:01 -0700

    handle a situation when malloc fails to allocate memory

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -32,7 +32,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int total_size = name_size + owner_size + 2;
 	dog_t *dog = malloc(sizeof(age) + (total_size) * sizeof(char));
 
-	if (!name || !owner)
+	if (dog)
 		return (NULL);
 	dog->name = name;
 	dog->age = age;
