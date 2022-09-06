857e1dc0c5e4b9bfeaccc8b8537f7d4e1143537a
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-09-06 03:05:34 -0700

    Use heap to create a memory location for storing a structure that will continue to exist even if the stack is cleared

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,41 @@
+#include "dog.h"
+#include <stddef.h>
+#include "stdlib.h"
+
+/**
+  * _strlen - calculates string length
+  * @str: address of the string
+  * Return: string length
+  */
+
+int _strlen(char *str)
+{
+	int string_length = 0;
+
+	while (*str)
+		string_length++, str++;
+	return (string_length);
+}
+
+/**
+  * new_dog - creates a new dog
+  *
+  * @name: dog's name
+  * @age: dog's age
+  * @owner: dog's owner
+  * Return: a new dog
+  */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int name_size  = _strlen(name), owner_size = _strlen(owner);
+	int total_size = name_size + owner_size + 2;
+	dog_t *dog = malloc(sizeof(age) + (total_size) * sizeof(char));
+
+	if (!name || !owner)
+		return (NULL);
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
