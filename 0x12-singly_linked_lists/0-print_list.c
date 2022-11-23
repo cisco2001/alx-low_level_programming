2103fd257aba2d4911a60ac62f9523c3706d07a4
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-11-23 12:56:24 +0300

    everything is ok

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of list_t list
+ * @h: pointer to the first node in the list
+ *
+ * Return: number of nodes in a list
+ */
+size_t print_list(const list_t *h)
+{
+	int number_of_nodes = 0;
+
+	while (h)
+	{
+		if (h->str)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[%d] %s\n", 0, "nil");
+		h = h->next;
+		number_of_nodes++;
+	}
+	return (number_of_nodes);
+}
