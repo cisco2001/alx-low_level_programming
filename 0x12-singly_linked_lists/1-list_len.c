63ba0c48153abdcbee205a8a8ae4b02abff681b7
Mtwa Johakim Mgimwa <mtwa2001@gmail.com>
2022-11-23 13:37:16 +0300

    happy counting elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "stddef.h"
+#include "lists.h"
+
+/**
+ * list_len - calculates number of elements in a list
+ * @h: pointer to the first node in the list
+ *
+ * Return: number of elements in a linked list_t list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t number_of_elements = 0;
+
+	while (h)
+	{
+		number_of_elements++;
+		h = h->next;
+	}
+	return (number_of_elements);
+}
+
